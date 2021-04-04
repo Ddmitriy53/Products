1bef58482b7c50f8e2d291e723afc4ff9281ac58
Denysenko Dmitriy <denysenkodmitriy53@gmail.com>
2021-04-04 16:25:53 +0300

    Second task 1.2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-$('.js-tab-trigger').click(function () {
-  let id = $(this).attr('data-tab'),
-    content = $('.js-tab-content[data-tab="' + id + '"]');
-
-  $('.js-tab-trigger.active').removeClass('active');
-  $(this).addClass('active');
-
-  $('.js-tab-content.active').removeClass('active');
-  content.addClass('active');
-});
-
 $(document).ready(function () {
+
+  $('.js-tab-trigger').click(function () {
+    $(this).addClass('active').siblings().removeClass('active')
+      .closest('.product-tabs').find('.js-tab-content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
   $('.terms__top').click(function () {
     $(this).toggleClass('terms-item--active').next().slideToggle();
     $('.terms__top').not(this).removeClass('terms-item--active').next().slideUp();
   });
+
+  $('#phone').mask('+38(000) 00 - 00 - 000', {
+    placeholder: "+38 (0xx) xx - xx - xxx"
+  });
+
 });
--- js/slider.js
+++ js/slider.js
@@ -16,17 +16,41 @@ $(document).ready(function () {
     nextArrow: '<button id="next" type="button" class="product-slider__button-next"><svg class="product-slider__next-img" width="16" height="29"><use href="icons/sprite.svg#arrow"/></svg></button>',
     asNavFor: '.product-info__slider'
   });
-});
-
-$(document).ready(function () {
-  const slidesCount = $('.products-card').length;
-  if (slidesCount > 5) {
-    $('.products-main').slick({
-      slidesToShow: 5,
-      slidesToScroll: 2,
-      focusOnSelect: true,
-      prevArrow: '<button id="cards-prev" type="button" class="cards-slider__button-prev"><svg class="card-slider__prev-img" width="16" height="29"><use href="icons/sprite.svg#arrow"/></svg></button>',
-      nextArrow: '<button id="cards-next" type="button" class="cards-slider__button-next"><svg class="card-slider__next-img" width="16" height="29"><use href="icons/sprite.svg#arrow"/></svg></button>',
-    });
-  }
+  $('.products-main').slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    focusOnSelect: true,
+    prevArrow: '<button id="cards-prev" type="button" class="cards-slider__button-prev"><svg class="card-slider__prev-img" width="16" height="29"><use href="icons/sprite.svg#arrow"/></svg></button>',
+    nextArrow: '<button id="cards-next" type="button" class="cards-slider__button-next"><svg class="card-slider__next-img" width="16" height="29"><use href="icons/sprite.svg#arrow"/></svg></button>',
+    responsive: [{
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 4,
+          slidesPerRow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 650,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
